d7d03cde9023e8d725718fd2167b4870b90527bf
paradoxical-dev <jaredonnell21@gmail.com>
2025-07-09 16:44:46 -0500

    feat(init): added picker functionality for session loading
    Added a base picker using vim.ui.select. Will need to add options for custom snacks picker as well

--- lua/multi-session/init.lua
+++ lua/multi-session/init.lua
@@ -4,6 +4,8 @@ local session_dir = vim.fn.stdpath("state") .. "/multi-session"
 
 local uv = vim.uv or vim.loop
 
+M.config = {}
+
 M.project_list = function()
     local dirs = {}
     local fd = uv.fs_scandir(session_dir)
@@ -23,15 +25,64 @@ M.project_list = function()
     return dirs
 end
 
-M.pick_project = function()
-    vim.ui.select(M.project_list(), { prompt = "Select project" }, function(choice)
-        print(choice)
-    end)
+M.session_list = function(project)
+    local files = {}
+    local fd = uv.fs_scandir(session_dir .. "/" .. project)
+    if not fd then
+        return files
+    end
+    while true do
+        local name, type = uv.fs_scandir_next(fd)
+        if not name then
+            break
+        end
+        if type == "file" then
+            table.insert(files, name)
+        end
+    end
+    return files
 end
 
-M.path = function()
-    local cwd = uv.cwd()
-    local current_dir = vim.fn.fnamemodify(cwd, ":t")
+M.pick_session = function(project)
+    if M.config.picker == "snacks" then
+        local sel = M.snacks_picker("sessions")
+        return sel
+    else
+        local co = coroutine.running()
+        vim.ui.select(M.session_list(project), { prompt = "Select session" }, function(choice)
+            coroutine.resume(co, choice)
+        end)
+        return coroutine.yield()
+    end
+end
+
+M.pick_project = function()
+    if M.config.picker == "snacks" then
+        local sel = M.snacks_picker("projects")
+        return sel
+    else
+        local co = coroutine.running()
+        vim.ui.select(M.project_list(), { prompt = "Select project" }, function(choice)
+            coroutine.resume(co, choice)
+        end)
+        return coroutine.yield()
+    end
+end
+
+M.test = function()
+    coroutine.wrap(function()
+        local project = M.pick_project()
+        print(project)
+    end)()
+end
+
+M.load = function()
+    coroutine.wrap(function()
+        local project = M.pick_project()
+        local session = M.pick_session(project)
+
+        vim.cmd("source " .. session_dir .. "/" .. project .. "/" .. session)
+    end)()
 end
 
 M.delete = function()
@@ -47,10 +98,6 @@ M.save = function()
         vim.fn.mkdir(project, "p")
     end
     vim.cmd("mksession! " .. project .. "/session.vim")
-end
-
-M.load = function()
-    vim.cmd("source " .. session_dir .. "/session.vim")
 end
 
 function M.setup(opts)
